9fe0e9556a79944d00de9a81d6eaf6cb9b9e27b6
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-02-25 21:29:01 +0530

    added auth js

--- auth.ts
+++ auth.ts
@@ -1,14 +1,11 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
 
-export const authOptions = {
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    GitHub({
+      clientId: process.env.AUTH_SECRET_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
-export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
+})
