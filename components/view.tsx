7914222d4744598b6cc10065280d03162b6d3ae6
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-03-01 01:10:41 +0530

    creating page for single startups

---
+++ components/view.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Ping from "@/components/ping"
+
+const view = () => {
+  return (
+    <div className='view-container'>
+        <div className='absoulute -top-2 -right-2'>
+            <Ping/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default view
